71af4ea3c4a838e645402ed431982c12fed5d183
The Fox <lisieckikamil93@gmail.com>
2024-06-25 22:54:12 +0200

    feat: move id auth to env

--- client-project/src/helpers/supabaseClient.ts
+++ client-project/src/helpers/supabaseClient.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(
-  "https://pprakrwwprhcswonwict.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwcmFrcnd3cHJoY3N3b253aWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzMzkwNTAsImV4cCI6MjAzNDkxNTA1MH0.WtciQZUy95YjzcXLnoQUoldCs_USMt95sn4pbXeytJU"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing environment variables for Supabase");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
