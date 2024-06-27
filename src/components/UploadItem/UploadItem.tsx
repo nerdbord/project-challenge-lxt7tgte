eacd654dad2fdf0bf77e5b6cd7b30c4103a94ff9
The Fox <lisieckikamil93@gmail.com>
2024-06-27 13:44:58 +0200

    feat: images by logged user show in UploadItem file

--- src/components/UploadItem/UploadItem.tsx
+++ src/components/UploadItem/UploadItem.tsx
@@ -1,13 +1,42 @@
+import { useAppStore } from "../../store";
 import styles from "./UploadItem.module.css";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useEffect, useState } from "react";
 
 const UploadItem = () => {
+  const { images, uploadedImageUrl } = useAppStore();
+  const user = useUser();
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    if (!user) {
+      setMessage("user not exist");
+    }
+  });
+
+  const CDNURL =
+    "https://pprakrwwprhcswonwict.supabase.co/storage/v1/object/public/images/";
+
   return (
     <div className={styles.item}>
-      <p>File Name</p>
-      <div className={styles.buttonbox}>
-        <button>Delete</button>
-        <button>Download</button>
-      </div>
+      <>
+        {images.map((x) => {
+          return (
+            <div key={x.name}>
+              <img
+                className={styles.img}
+                src={`${CDNURL}${user?.id}/${x.name}`}
+                alt={x.name}
+              />
+              <div>
+                <button>Delete</button>
+                <button>Download</button>
+              </div>
+            </div>
+          );
+        })}
+      </>
+      <div className={styles.buttonbox}></div>
     </div>
   );
 };
