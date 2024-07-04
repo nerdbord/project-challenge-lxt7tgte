6f8d676f89d34bc57a50518731a7314653fe54e9
The Fox <lisieckikamil93@gmail.com>
2024-07-04 22:32:03 +0200

    feat: add notistack

--- src/components/UploadItems/UploadItems.tsx
+++ src/components/UploadItems/UploadItems.tsx
@@ -4,12 +4,14 @@ import styles from "./UploadItems.module.css";
 import { useUser } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
 import { FileObject } from "@supabase/storage-js";
+import { SnackbarProvider, useSnackbar } from "notistack";
 
 const UploadItems = () => {
   const { images, setImages } = useAppStore();
   const user = useUser();
   const [message, setMessage] = useState<string>("");
   // const [urlClipboard, setUrlClipboard] = useState<string>("");
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     if (!user) {
@@ -40,21 +42,25 @@ const UploadItems = () => {
     const { error } = await supabase.storage
       .from("images")
       .remove([user?.id + "/" + imageName]);
+    enqueueSnackbar("Image has been successfully deleted", {
+      variant: "success",
+    });
 
     if (error) {
-      alert("failed: delete image");
+      enqueueSnackbar("Something goes wrong", {
+        variant: "error",
+      });
     } else {
       getImages();
     }
   }
 
-  //
   const copyToClipboard = (url: string) => {
     navigator.clipboard.writeText(url);
-    alert("URL copied to clipboard");
+    enqueueSnackbar("URL copied to clipboard", {
+      variant: "success",
+    });
   };
-
-  //
 
   const CDNURL =
     "https://pprakrwwprhcswonwict.supabase.co/storage/v1/object/public/images/";
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "./helpers/supabaseClient.ts";
+import { SnackbarProvider, useSnackbar } from "notistack";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SessionContextProvider supabaseClient={supabase}>
-      <App />
+      <SnackbarProvider>
+        {" "}
+        <App />
+      </SnackbarProvider>
     </SessionContextProvider>
   </React.StrictMode>
 );
