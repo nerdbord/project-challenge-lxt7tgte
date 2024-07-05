4d624c05ea1ab2dd5fa149310d167e51de3e5ae1
The Fox <lisieckikamil93@gmail.com>
2024-07-05 17:50:55 +0200

    fix: error for vercel dep

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "./helpers/supabaseClient.ts";
-import { SnackbarProvider, useSnackbar } from "notistack";
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
